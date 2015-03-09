e4b352d530ae2b63e362bb833f385835ffcd6ea5
Yuchen Yang <yuchenyang2013@u.northwestern.edu>
2015-03-09 15:19:18 -0500

    attack and spell animation v1

---
+++ Assets/C# Scripts/Combat/BasicAttackProjectile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAttackProjectile : MonoBehaviour {
+	
+	public float velocity = 15f;
+	public Vector3 targetLocation;
+	private Vector3 velocityVector;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		velocityVector = targetLocation - transform.localPosition;
+		//velocityVector.z = 0f;
+		velocityVector.Normalize();
+		transform.localPosition += velocityVector * velocity * Time.deltaTime;
+		
+		ArrivedAtTargetCheck ();
+	}
+	
+	void ArrivedAtTargetCheck()
+	{
+		velocityVector = targetLocation - transform.localPosition;
+		if (velocityVector.magnitude < 1f)
+		{
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+
+}
