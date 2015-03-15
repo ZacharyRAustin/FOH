7d49fe1ba55998450ee12a3c42721210194ff0ce
Zachary Austin <zacharyraustin@gmail.com>
2015-03-15 13:28:48 -0500

    working on generated item test

---
+++ Assets/C# Scripts/Utilities/GeneratedItemTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneratedItemTest{
+    public static void PerformTest() {
+        WriteFile f = new WriteFile(@"C:\users\zachary\desktop\items.txt");
+        for(int i = 1; i < 100; i++)
+        {
+            List<int> numGenerated = new List<int>();
+            List<string> items = new List<string>();
+
+            for(int j = 0; j < 10; j++)
+            {
+                SpawnCharacteristics.testLeveling(i);
+            }
+        }
+    }
+}
