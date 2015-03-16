6b3f064be8a5f642241bdd42ba1c0edeac297875
KushDingies <thekush93@gmail.com>
2015-03-16 14:09:03 -0500

    3/16 v2
    Music, bug fixes, etc

---
+++ Assets/C# Scripts/Utilities/AudioManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip mainClip;
+	public AudioClip fightClip;
+	public AudioClip bossClip;
+	public AudioClip gameOverClip;
+	public AudioClip softClip;
+	public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Awake () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void BossSong()
+	{
+		audioSource.Stop ();
+		audioSource.clip = bossClip;
+		audioSource.Play ();
+	}
+
+	public void FightSong()
+	{
+		audioSource.Stop ();
+		audioSource.clip = fightClip;
+		audioSource.Play ();
+	}
+
+	public void GameOverSong()
+	{
+		audioSource.clip = gameOverClip;
+		audioSource.Play ();
+	}
+
+	public void MainSong()
+	{
+		audioSource.Stop ();
+		audioSource.clip = mainClip;
+		audioSource.Play ();
+	}
+
+	public void SoftSong()
+	{
+		audioSource.Stop ();
+		audioSource.clip = softClip;
+		audioSource.Play ();
+	}
+
+	public void RandSong()
+	{
+		int x = Random.Range (0, 3);
+
+		if (x == 0)
+		{
+			MainSong();
+		}
+		else if (x == 1)
+		{
+			FightSong();
+		}
+		else
+		{
+			SoftSong();
+		}
+	}
+}
---
+++ Assets/C# Scripts/Utilities/GeneratedItemTest.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneratedItemTest{
+    public static void PerformTest() {
+        WriteFile f = new WriteFile(@"C:\users\zachary\desktop\items.txt");
+        for(int i = 1; i < 100; i++)
+        {
+            List<string> items = new List<string>();
+
+            for(int j = 0; j < 10; j++)
+            {
+                SpawnCharacteristics.setAvgEnemyLevel(i);
+                DropSystem.GenerateLoot();
+                
+                List<RandomAbility> abilities = DropSystem.getGeneratedAbilities();
+                for(int k = 0; k < abilities.Count; k++)
+                {
+                    items.Add("Ability: " + abilities[k].name);
+                }
+                List<Equipment> equip = DropSystem.getGeneratedEquipment();
+                for (int h = 0; h < equip.Count; h++)
+                {
+                    items.Add("Item: " + equip[h].name + " with level " + equip[h].level);
+                }
+                if(items.Count != 0)
+                {
+                    f.writeLine("For Level: " + i + " iteration " + j);
+                    f.writeLine("Average Enemy Level: " + SpawnCharacteristics.getAvgEnemyLevel());
+                    items.Add("\n\n");
+                    f.writeAllLines(items);
+                }
+
+                items.Clear();
+            }
+        }
+    }
+}
