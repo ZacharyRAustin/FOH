a779ed29a8061d317aa9ff000cb5cf892e22df4d
aakashmishra <aakashmishra2016@u.northwestern.edu>
2015-03-16 00:15:17 -0500

    some more changes

--- Assets/C# Scripts/Character/Character.cs
+++ Assets/C# Scripts/Character/Character.cs
@@ -177,7 +177,7 @@ public class Character : MonoBehaviour
 				//GUI.TextArea (new Rect (0,0,60, size.y), character.name);
 				//draw the filled-in part:
 				GUI.BeginGroup(new Rect(0, 0, width_x , size.y));
-				GUI.Box(new Rect(0, 0, width_x, size.y), new GUIContent(""), style_experience);
+				GUI.Box(new Rect(0, 0, width_x * (stats.CurrentExp)/(stats.MaxExp), size.y), new GUIContent(""), style_experience);
 				GUI.EndGroup();
 				GUI.EndGroup();
 
